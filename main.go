acf00824ddda503aac280da98089b74424ccd5e6
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2021-04-05 18:35:09 -0500

    NOT READING FILES

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
     "fmt"
     "strings"
+    "os"
+    "io/ioutil"
+    "log"
 )
 //Main for Main.go
+/*Map designated to save the equivalences in both alphabets*/
 var diccionario= map[string]string{"A":".-", "B":"-...", 
 "C":"-.-.", "CH": "----", "D": "-..", "E":".", "F":"..-.",
 "G":"--.", "H":"....", "I":"..", "J":".---", "K":"-.-", 
@@ -14,20 +18,93 @@ var diccionario= map[string]string{"A":".-", "B":"-...",
 "0":"-----", "1":".----", "2":"..---", "3":"...--", "4":"....-",
 "5":".....", "6":"-....", "7":"--...", "8":"---..","9":"----.",
 ".":".-.-.-", ",":"--..--", "?":"..--..", "\"":".-..-."}
+/*
+*
+*/
+
 
 
 func main(){
-	var saludoMorse string= from_arabic_to_morse("HOLA PAPI")
-	var saludoNoMorse string= from_morse_to_arabic(saludoMorse)
-	fmt.Println(saludoMorse)
-	fmt.Println("===================================")
-	fmt.Println(saludoNoMorse)
+	var files []string=reader_num_file()
+	fmt.Print(files)
+	checker(files)
+	fmt.Println("Finish checker")
+	apply_for_each_file(files)
 }
 
-func out_text(text []string) string{
-	var outed string =strings.Join(text," ")
-	return outed
+
+func apply_for_each_file(files []string){
+	for x := range files{
+
+		if files[x]!= "main.go"{
+		var toWrite= encrypt(files[x])
+		write_File(files[x], toWrite)
+	
+		}
+	}
 }
+
+func reader_num_file()[]string{
+	var file []string =os.Args
+	return file
+}
+
+/**
+*Check if each file is correct or exists
+*/
+func checker(files []string){
+	for x := range files{
+		if _, err := os.Stat(files[x]); err==nil{
+			print("---")
+		}else if os.IsNotExist(err){
+			print(files[x], "No Such file or directory")
+		
+		}
+	}
+}
+
+/**
+*Encrypt the file and return the message converted to Morse
+*
+*/
+func encrypt(files string)string{
+	fmt.Println("Encriptando")
+		content, err := ioutil.ReadFile(files)
+		if err != nil {
+			log.Fatal(err)
+		}
+		/*Read file*/
+
+	var knowledge string=from_arabic_to_morse(strings.ToUpper(string(content)))
+	return knowledge
+
+}
+func check(e error){
+	if e != nil{
+		panic(e)
+	}
+
+
+}
+
+func write_File(file string,content string){
+
+f, err :=os.Create("m-"+file)
+b:= []byte(content)
+n2, err:= f.Write(b)
+check(err)
+fmt.Println("Wrote in " , n2)
+defer f.Close()	
+
+
+}
+
+
+
+
+
+
+
 /*
 *Golang has not a function built to get the key from a value as java
 *To solve this uniquely we will use an obvios solution, iterate each key
